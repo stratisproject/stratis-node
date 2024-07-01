f07c4e49605295abbb14a7fe113e4e5ae95cd058
SergeevDmitry <sergeevdms@gmail.com>
2024-07-01 19:37:31 +0300

    Prevent user to continue installation on unsupported OS

--- launcher/src/components/UI/welcome-page/components/WelcomeBody.vue
+++ launcher/src/components/UI/welcome-page/components/WelcomeBody.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="col-start-3 col-end-23 row-start-4 row-span-6 grid grid-cols-12 grid-rows-6 p-4 bg-[#1E2429] rounded-lg">
+  <div v-if="isSupported" class="col-start-3 col-end-23 row-start-4 row-span-6 grid grid-cols-12 grid-rows-6 p-4 bg-[#1E2429] rounded-lg">
     <div class="col-start-1 col-end-13 row-start-1 row-end-2 w-full h-full flex justify-center items-center">
       <span class="text-xl text-gray-300 font-normal text-center">{{ $t("welcomePage.chooseInstallNode") }}</span>
     </div>
@@ -7,10 +7,35 @@
       <CardItem v-for="item in welStore.installationOptions" :key="item" :item="item" />
     </div>
   </div>
+  <div v-if="!isSupported" class="col-start-3 col-end-23 row-start-4 row-span-6 grid grid-cols-12 grid-rows-6 p-4 bg-[#1E2429] rounded-lg">
+    <div class="col-start-1 col-end-13 row-start-1 row-end-7 w-full h-full flex justify-evenly items-center space-x-2 text-white">
+      UNSUPPORTED OS
+    </div>
+  </div>
 </template>
 <script setup>
+import ControlService from "@/store/ControlService";
 import CardItem from "./CardItem.vue";
+import { ref, onMounted } from "vue";
 import { useWelcomeStore } from "@/store/welcomePage";
 
 const welStore = useWelcomeStore();
+const isSupported = ref(false);
+
+// Lifecycle Hooks
+onMounted(() => {
+  checkSupportedOs();
+});
+
+const checkSupportedOs = async () => {
+  const osResponse = await ControlService.checkOS();
+  const osData = await osResponse
+  const osName = osData && osData.hasOwnProperty("name") && osData.name ? osData.name : "";
+  const osVers = osData && osData.hasOwnProperty("version") && osData.version ? osData.version : "";
+  if (osName === "Ubuntu" && (osVers === "22.04" || osVers === "24.04")) {
+    isSupported.value = true
+  } else {
+    isSupported.value = false
+  }
+};
 </script>
